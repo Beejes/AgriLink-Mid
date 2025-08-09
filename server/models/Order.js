d86532eb6acff5c929c14d5b9eb091961d024c5f
Beejes <dbijesh6@gmail.com>
2025-08-09 15:14:12 +0545

    Initial commit

---
+++ server/models/Order.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    userId: {type: String, require: true, ref: 'user'},
+    items: [{
+        product: {type: String, require: true, ref: 'product'},
+        quantity: {type: String, require: true}
+    }],
+    amount: {type: Number, require: true},
+    address: {type: String, require: true, ref: 'address'},
+    status: {type: String, default: 'Order Placed'},
+    paymentType: {type: String, require: true},
+    isPaid: {type: Boolean, require: true, default: false},
+
+}, {timestamps: true })
+
+const Order = mongoose.models.Order || mongoose.model('order', orderSchema)
+
+export default Order
